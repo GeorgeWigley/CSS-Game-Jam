e2c8a2c436deb796981a8ec6e1ca7ff2eb95751c
Matthew Swann <cv20549@bristol.ac.uk>
2022-11-03 10:49:50 +0000

    smasher works

--- Assets/Scripts/GlassBreaking/GlassBehaviour.cs
+++ Assets/Scripts/GlassBreaking/GlassBehaviour.cs
@@ -12,31 +12,32 @@ public class GlassBehaviour : MonoBehaviour
 
     public bool updateTexture;
 
-// Start is called before the first frame update
-void Start()
-{
-    timesSmashed = 0;
-    updateTexture = false;
-    smashedThisReset = false;
-}
-
-// Update is called once per frame
-void Update()
-{
-    if (updateTexture) {
+    // Start is called before the first frame update
+    void Start()
+    {
+        timesSmashed = 0;
         updateTexture = false;
-        GetComponent<Renderer>().material.mainTexture = textures[timesSmashed];
+        smashedThisReset = false;
     }
-}
 
-public void smash() {
-    if (!smashedThisReset) {
-        timesSmashed += 1;
-        updateTexture = true;
-        smashedThisReset = true;
-        if (timesSmashed == 5) {
-            // replace object
+    // Update is called once per frame
+    void Update()
+    {
+        if (updateTexture) {
+            updateTexture = false;
+            GetComponent<Renderer>().material.mainTexture = textures[timesSmashed];
         }
     }
-}
+
+    void OnCollisionEnter (Collision targetObj) {
+        if (!smashedThisReset && targetObj.gameObject.tag == "Smasher")
+        {
+            timesSmashed += 1;
+            updateTexture = true;
+            smashedThisReset = true;
+            if (timesSmashed == 5) {
+                // replace object
+            }
+        }
+    }
 }
